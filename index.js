7e122397ba128474168d933fcc55af7d0dcacc35
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-09 16:59:08 +0100

    files deleted

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const server = require("./server");
 
-// server.listen(5501, () => {
-//   console.log(`Google Clone Listening On Port 5501`);
-// });
-
-
-server.listen(process.env.PORT || 5501, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+server.listen(5501, () => {
+  console.log(`Google Clone Listening On Port 5501`);
 });
+
+
