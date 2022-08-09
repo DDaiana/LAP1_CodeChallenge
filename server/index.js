5b4dc74e0408f8080413c06bd3553f1360677016
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-09 15:39:12 +0100

    server changed for index file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require("./server");
 
-server.listen(3001, () => {
-  console.log(`Google Clone Listening On Port 3001`);
+server.listen(5501, () => {
+  console.log(`Google Clone Listening On Port 5501`);
 });
