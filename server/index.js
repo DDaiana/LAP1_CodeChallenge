0886f335d7dd7540ecd0a6efd4b5ff30510ff196
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-09 15:35:37 +0100

    docker ignore added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require("./server");
 
-server.listen(3500, () => {
-  console.log(`Google Clone Listening On Port 3500`);
+server.listen(3001, () => {
+  console.log(`Google Clone Listening On Port 3001`);
 });
