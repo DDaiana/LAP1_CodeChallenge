e957fe90120c08cb3f0117e84c8df130b6caf9a9
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-09 16:16:32 +0100

    server changed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require("./server");
 
-server.listen(5501, () => {
-  console.log(`Google Clone Listening On Port 5501`);
+// server.listen(5501, () => {
+//   console.log(`Google Clone Listening On Port 5501`);
+// });
+
+
+server.listen(process.env.PORT || 5501, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
