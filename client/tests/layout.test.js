969278dfd04dbefaec4867bc85392e4242a593b2
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-12 02:03:29 +0100

    helpers file added

--- client/tests/layout.test.js
+++ client/tests/layout.test.js
@@ -1,16 +1,28 @@
-/** @jest-environment jsdom */
+const renderDOM = require('./helpers');
+let dom;
+let document;
 
- const fs = require('fs');
- const path = require('path');
- const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+//  const fs = require('fs');
+//  const path = require('path');
+//  const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
  
  
  describe('index.html', () => {
-     beforeEach(() => {
-         document.documentElement.innerHTML = html.toString();
-     })
+    //  beforeEach(() => {
+    //      document.documentElement.innerHTML = html.toString();
+    //  })
  
+    beforeEach( async () => {
+        dom = await renderDOM('index.html');
+        document = await dom.window.document
+        })
+
      test('it has a title', () => {
+        let header = document.querySelector('title');
+        expect(header.textContent).toBeTruthy();
+    })
+
+     test('it has Homepage as title', () => {
          let header = document.querySelector('title');
          expect(header.textContent).toContain('Homepage');
      })
